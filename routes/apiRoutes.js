f62f87dde0f75a26ccd85006f22aa59535ab78f9
aumcintyre <aumcintyre@gmail.com>
2022-10-03 12:57:32 -0500

    Notes save on Live Server, but not on Heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
     //Reads the db.json file to pull any saved notes down as objects
 
     app.get('/api/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../db/db.json'))
+        res.sendFile(path.join(__dirname, '../db/db.json'));
     });
 
 
