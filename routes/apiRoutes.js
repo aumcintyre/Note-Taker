b790e6e4edc00d50387824857711aa8c8e412d8e
aumcintyre <aumcintyre@gmail.com>
2022-10-03 12:44:09 -0500

    Working on pushing/writing user inputted notes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,10 +7,34 @@ var uniqid = require('uniqid');
 
 module.exports = (app) => {
 
-//Reads the db.json file to pull any saved notes down as objects
+    //Reads the db.json file to pull any saved notes down as objects
 
     app.get('/api/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../db/db.json'))
-    })
+    });
+
+
+    //The Post method will receive a new note, which will be constructed as a JSON object given the body below
+    app.post('/api/notes', (req, res) => {
+        var db = fs.readFileSync('db/db.json');
+        db = JSON.parse(db);
+        res.json(db);
+
+        //Note body template in JSON format
+        let newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uniqid(),
+        }
+
+        //Push the new note to the UserNote array (Set empty by default). Note will then be written to the DB.json file. This will add the new note and rewrite the file in order to avoid adding duplicate notes.
+
+        db.push(newNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(db);
+    });
+
+    //Function to delete notes will be added here.
+
 
 }
