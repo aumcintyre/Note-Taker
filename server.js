20714c34943033c108409dd201050bc2be2060a4
aumcintyre <aumcintyre@gmail.com>
2023-01-19 13:44:37 -0600

    changing heroku port settings

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 
 
 //Create static routes for each file in the "public" folder
